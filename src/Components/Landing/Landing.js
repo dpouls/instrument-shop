162992b621c0177eb601d58d0af4cb03dd837a8a
mattcbodily <mattcbodily@gmail.com>
2020-10-22 11:48:01 -0600

    added styling to landing

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import drumImage from '../../assets/cajon-dummy-image.png';
 import './Landing.scss';
 
 export default props => (
     <main>
-        <section>
-            
+        <section className='intro-section'>
+            <div>
+                <h1>Text Here</h1>
+                <p>And even more text here</p>
+            </div>
+            <img src={drumImage} alt='Cajon Drum' className='landing-image'/>
         </section>
-        <section>
-
+        <section className='selling-points-section'>
+            <h1>What we offer</h1>
+            <section className='selling-points-flex'>
+                <div className='selling-point'></div>
+                <div className='selling-point'></div>
+                <div className='selling-point'></div>
+            </section>
         </section>
     </main>
 )
