65e2fd84db8536e08ee86eed3c2a3d4914376f01
mattcbodily <mattcbodily@gmail.com>
2020-10-22 10:56:11 -0600

    added node-sass

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import './Landing.scss';
 
 export default props => (
     <main>
-        
+        <section>
+            
+        </section>
+        <section>
+
+        </section>
     </main>
 )
