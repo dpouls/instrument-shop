2a7b611e062d2f991668b8fffe56ac314f4e269a
mattcbodily <mattcbodily@gmail.com>
2020-10-22 11:28:22 -0600

    basic header styling built

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import './Header.scss';
 
 export default props => (
-    <header>
-        Header
+    <header className='main-header'>
+        <h1>ShopName</h1>
+        <nav>
+            <Link to='/' className='nav-link'>Home</Link>
+            <Link to='/shop' className='nav-link'>Shop</Link>
+            <Link to='/custom' className='nav-link'>Custom</Link>
+            <Link to='/about' className='nav-link'>About Us</Link>
+        </nav>
     </header>
 )
